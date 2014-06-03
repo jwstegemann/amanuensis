d1c53c06782dee05ee67a5f55759301511bab9ff
Jens Stegemann <jwstegemann@gmail.com>
2014-06-03 21:17:00 +0200

    repaired create_user-script

--- curl/admin/create_user.sh
+++ curl/admin/create_user.sh
@@ -16,14 +16,14 @@ query='{
   "params": {
     "login": "$3",
     "pwd": "1eb1e1d6ad08dcf1a1020ae999d24ee836b30f0987acf3934e18783cc551bfc",
-    "name": "$4"
+    "name": "$4",
     "lang": "$5"
   }
 }'
 
 replacedQuery=`echo $query | sed "s/\\$3/$3/" | sed "s/\\$4/$4/" | sed "s/\\$5/$5/"`
 
-curl -H "accept: applicaton/json" -H "X-Stream:true" -H "content-type:application/json" "$1/db/data/cypher" -d "$replacedQuery"
+curl -s -H "accept: applicaton/json" -H "X-Stream:true" -H "content-type:application/json" "$1/db/data/cypher" -d "$replacedQuery"
 
 
 
